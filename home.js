8968ba106131214d8d1e3f1f00df550d31d4d15e
Adesina Surprise <adesinasurprise2006@gmail.com>
2024-02-05 11:46:46 +0100

    This my third commit

--- home.js
+++ home.js
@@ -2,11 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const searchIcon = document.getElementById("search-icon");
   const searchInput = document.getElementById("search-input");
 
-  searchIcon.addEventListener("click", function () {
-    window.location.href = "search.html"; // Navigate to search page
-  });
+  // Check if searchIcon and searchInput elements exist before adding event listeners
+  if (searchIcon && searchInput) {
+    searchIcon.addEventListener("click", function () {
+      window.location.href = "search.html"; // Navigate to search page
+    });
 
-  searchInput.addEventListener("click", function () {
-    window.location.href = "search.html"; // Navigate to search page
-  });
+    searchInput.addEventListener("click", function () {
+      window.location.href = "search.html"; // Navigate to search page
+    });
+  } else {
+    console.error("Search icon or search input not found.");
+  }
 });
