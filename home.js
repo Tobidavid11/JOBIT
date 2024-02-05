adea595a99ac20417be64e79f233c28b9fd4e353
Adesina Surprise <adesinasurprise2006@gmail.com>
2024-02-05 10:43:13 +0100

    This is jobit second commit

---
+++ home.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const searchIcon = document.getElementById("search-icon");
+  const searchInput = document.getElementById("search-input");
+
+  searchIcon.addEventListener("click", function () {
+    window.location.href = "search.html"; // Navigate to search page
+  });
+
+  searchInput.addEventListener("click", function () {
+    window.location.href = "search.html"; // Navigate to search page
+  });
+});
